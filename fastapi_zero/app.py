011f37617d342531acb63bc194e91f2453344455
ghabryeu <gabiconstantino2001@gmail.com>
2025-07-23 18:31:48 -0300

    Adicionando rotas CRUD

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,22 +1,72 @@
-from fastapi import FastAPI
 from http import HTTPStatus
-from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fastapi import FastAPI, HTTPException
 
+from fastapi_zero.schemas import (
+    Message,
+    UserDB,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
 
-@app.get('/exercicio-html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
-def exercicio_aula_02():
-    return """
-    <html>
-      <head>
-        <title>Nosso olá mundo!</title>
-      </head>
-      <body>
-        <h1>olá, mundo!</h1>
-      </body>
-    </html>"""
+app = FastAPI(title='Minha API')
+
+database = []
 
 
-#def read_root():
-    #return {'message': 'olá, mundo!'}
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+def read_root():
+    return {'message': 'olá, mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+        id=len(database) + 1,
+    )
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put(
+    '/users/{user_id}', response_model=UserPublic
+    )
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User not found'
+        )
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}',
+    status_code=HTTPStatus.OK,
+    response_model=UserPublic
+    )
+def delete_user(user_id: int):
+
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User not found'
+        )
+
+    return database.pop(user_id - 1)
